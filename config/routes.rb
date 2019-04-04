3352d9c79a97566b6946560c9f743a7cae40995f
Chris Stull <chris.stull@gmail.com>
2019-04-04 19:25:15 -0400

    Final correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :schedules, only: [:show, :index, :destroy, :create] do
-    resources :appointments, only: [:show, :index, :destroy, :create]
+    resources :appointments, only: [:destroy, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
